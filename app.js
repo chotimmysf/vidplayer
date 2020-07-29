87c39bff476aa9946d9172ff5ba92d32346d3a2d
Timmy Cho <chotimmysf@gmail.com>
2020-07-28 21:40:18 -0700

    timestamp issue for innerHTML - formatting

--- app.js
+++ app.js
@@ -25,11 +25,25 @@ function updatePlayIcon() {
 // update progress & timestamp
 function updateProgress() {
   progress.value = (video.currentTime / video.duration) * 100;
+
+  // Get minutes
+  let mins = Math.floor(video.currentTime / 60);
+  if (mins < 10) {
+    mins = "0" + String(mins);
+  }
+
+  //Get seconds
+  let secs = Math.floor(video.currentTime % 60);
+  if (secs < 10) {
+    secs = "0" + String(secs);
+  }
+
+  timestamp.innerHTML = "$(mins):$(secs)";
 }
 
 // Set video time to progress
 function setVideoProgress() {
-  return true;
+  video.currentTime = (+progress.value * video.duration) / 100;
 }
 
 // Stop video
@@ -42,7 +56,7 @@ function stopVideo() {
 video.addEventListener("click", toggleVideoStatus);
 video.addEventListener("pause", updatePlayIcon);
 video.addEventListener("play", updatePlayIcon);
-video.addEventListener("timeupgrade", updateProgress);
+video.addEventListener("timeupdate", updateProgress);
 
 play.addEventListener("click", toggleVideoStatus);
 
